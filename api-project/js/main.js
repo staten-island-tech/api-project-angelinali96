ed5f81463a5672f3d15cbeb8cc3a9b27a253485f
angelinali96 <136626552+angelinali96@users.noreply.github.com>
2023-11-29 10:13:06 -0500

    initial commit

---
+++ api-project/js/main.js
@@ -0,0 +1,10 @@
+import '../css/style.css';
+import '../css/variables.css';
+import './api.js';
+
+
+//API PROJECT
+// bus api;  double screen
+// https://bustime.mta.info/api/stops-on-route-for-direction?routeId=MTA+NYCT_B8&directionId=1
+// fetch id and fill https://bustime.mta.info/m/index?q= id
+// route list https://bustime.mta.info/m/routes/
