51f533b52ec8e031c91215101ccb559e0c2c3fd2
angelinali96 <136626552+angelinali96@users.noreply.github.com>
2023-12-01 13:42:58 -0500

    finish bus selection

--- api-project/js/api.js
+++ api-project/js/api.js
@@ -1,3 +1,5 @@
+import { DOMSelect } from './dom';
+
 // fetch api
 const proxy = 'https://corsproxy.io/?'; // ik we werent supposed to use cors apis but i really wanted to make this project
 const url = `https://bustime.mta.info/api/stops-on-route-for-direction?routeId=MTA+NYCT_B1&directionId=1`;
@@ -22,6 +24,7 @@ function fetchToHtml(data){
         const list = parser.parseFromString(data, "text/html");
         const busList = list.querySelectorAll('ul.routeList li');
         console.log(busList);
+        busList.forEach(item => optionList(item));
 }
 
 async function getApi(url){
@@ -38,10 +41,11 @@ async function getApi(url){
 }
 getApi(url);
 
-// display results from api
+// display results from options
 function optionList(res){
-    DOMSelect.res.insertAdjacentHTML("afterbegin", `
-    <option value="${res.stops.id}">${res.stops.name}</option>
+    let list = res.querySelector('a').href.split("#", 2);
+    DOMSelect.options.insertAdjacentHTML("afterbegin", `
+    <option value="${list[1]}">${res.innerText}</option>
     `
-    ); // dont res.stops this should be applied in foreach
+    ); 
 }
--- api-project/js/dom.js
+++ api-project/js/dom.js
@@ -1,6 +1,7 @@
 const DOMSelect = {
     body: document.querySelector('body'),
     res: document.querySelector('.result'),
+    options: document.querySelector('select'),
     themer: document.querySelector('input[type="checkbox"]'),
 };
 
